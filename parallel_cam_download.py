f242806ae2919fcab8304f6e93a3781f132c1fe8
anup-mohan <mohan11@purdue.edu>
2014-02-16 07:43:16 +0000

    script to invoke download scripts in parallel

---
+++ parallel_cam_download.py
@@ -0,0 +1,7 @@
+import subprocess
+
+
+subprocess.Popen(["python", '/home/anupmohan/Documents/Research/camcam/test_mjpeg_stream_axis.py','128.10.29.33','cam1'])
+subprocess.Popen(["python", '/home/anupmohan/Documents/Research/camcam/test_mjpeg_stream_axis.py','128.104.138.26','cam2'])
+
+print("here")
